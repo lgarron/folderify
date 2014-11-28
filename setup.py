1557aab0d1f4d16b51926d362c402c37263064b8
Lucas Garron <code@garron.net>
2014-11-27 18:04:41 -0800

    Increment version to 0.0.7.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
   name="folderify",
-  version="0.0.6",
+  version="0.0.7",
   description="A tool to generate pretty, beveled OSX folder icons.",
   long_description=long_description,
   url="https://github.com/lgarron/folderify",
