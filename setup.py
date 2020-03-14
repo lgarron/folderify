2189b4a3866a32089b89725691cf267c80e6fa93
Lucas Garron <code@garron.net>
2020-03-13 17:02:42 -0700

    Bump version to 1.2.1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as f:
 
 setup(
     name="folderify",
-    version="1.2.0",
+    version="1.2.1",
     description="A tool to generate pretty, beveled OSX folder icons.",
     long_description=long_description,
     url="https://github.com/lgarron/folderify",
