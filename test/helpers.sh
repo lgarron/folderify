42465e18932a0f4592e087201287f74db8bc2450
Lucas Garron <code@garron.net>
2021-02-16 00:49:27 -0800

    Change indentation to two spaces.

--- test/helpers.sh
+++ test/helpers.sh
@@ -4,10 +4,10 @@ function check_file {
   FILE="${1}"
   if test -f "${FILE}"
   then
-      echo "✅ File ${FILE} exists as expected."
+    echo "✅ File ${FILE} exists as expected."
   else
-      echo "❌ File ${FILE} should exist, but doesn't."
-      exit 1
+    echo "❌ File ${FILE} should exist, but doesn't."
+    exit 1
   fi
 }
 
@@ -15,10 +15,10 @@ function check_folder {
   FILE="${1}"
   if test -d "${FILE}"
   then
-      echo "✅ Folder ${FILE} exists as expected."
+    echo "✅ Folder ${FILE} exists as expected."
   else
-      echo "❌ Folder ${FILE} should exist, but doesn't."
-      exit 1
+    echo "❌ Folder ${FILE} should exist, but doesn't."
+    exit 1
   fi
 }
 
@@ -26,9 +26,9 @@ function check_folder_icon {
   FILE="${1}/"
   if test -f "${FILE}"Icon$'\r'
   then
-      echo "✅ Folder ${FILE} has an icon as expected."
+    echo "✅ Folder ${FILE} has an icon as expected."
   else
-      echo "❌ Folder ${FILE} should have an icon, but doesn't."
-      exit 1
+    echo "❌ Folder ${FILE} should have an icon, but doesn't."
+    exit 1
   fi
 }
