795d8d9966a42ea875cceccb6ebacfc1f8cfda28
Lucas Garron <code@garron.net>
2021-02-16 02:05:34 -0800

    Update tests to add color scheme param tests.

--- test/test.sh
+++ test/test.sh
@@ -21,11 +21,20 @@ python -m folderify ./examples/src/apple.png .
 echo -e "\nCheck folder icon."
 check_folder_icon "."
 
-echo -e "\nTest that --verbose is accepted."
+echo -e "\nTest that \`--verbose\` is accepted."
 python -m folderify --verbose ./examples/src/apple.png
 
-echo -e "\nTest that --no-trim is accepted."
+echo -e "\nTest that \`--no-trim\` is accepted."
 python -m folderify --no-trim ./examples/src/apple.png
+
+echo -e "\nTest that \`--color-scheme auto\` is accepted."
+python -m folderify --color-scheme auto ./examples/src/apple.png
+
+echo -e "\nTest that \`--color-scheme light\` is accepted."
+python -m folderify --color-scheme light ./examples/src/apple.png
+
+echo -e "\nTest that \`--color-scheme dark\` is accepted."
+python -m folderify --color-scheme dark ./examples/src/apple.png
 
 for version in "10.5" "10.6" "10.7" "10.8" "10.9" "10.10" "10.11" "10.12" "10.13" "10.14" "10.15" "11.0"
 do
