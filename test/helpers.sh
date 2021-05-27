627be06d9e3e3eda344f205ab77b499c5ea8cf25
Craig Davison <crdaviso@cisco.com>
2021-05-27 00:00:57 -0600

    avoid nesting

--- test/helpers.sh
+++ test/helpers.sh
@@ -26,25 +26,22 @@ function check_folder_icon {
   FILE="${1}/"
   ICON="${FILE}"Icon$'\r'
   # Check for "custom icon" attribute on target
-  if xattr -p com.apple.FinderInfo "${FILE}" > /dev/null
+  if ! xattr -p com.apple.FinderInfo "${FILE}" > /dev/null
   then
-    # Check for "invisible" attribute on icon
-    if xattr -p com.apple.FinderInfo "${ICON}" > /dev/null
-    then
-      # Check for icns data in icon
-      if xattr -p com.apple.ResourceFork "${ICON}" > /dev/null
-      then
-        echo "✅ Folder ${FILE} has an icon as expected."
-      else
-        echo "❌ Folder ${FILE} icon should have a resource fork, but doesn't."
-        exit 1
-      fi
-    else
-      echo "❌ Folder ${FILE} icon should have a FinderInfo attribute, but doesn't."
-      exit 1
-    fi
-  else
     echo "❌ Folder ${FILE} should have a FinderInfo attribute, but doesn't."
     exit 1
   fi
+  # Check for "invisible" attribute on icon
+  if ! xattr -p com.apple.FinderInfo "${ICON}" > /dev/null
+  then
+    echo "❌ Folder ${FILE} icon should have a FinderInfo attribute, but doesn't."
+    exit 1
+  fi
+  # Check for icns data in icon
+  if ! xattr -p com.apple.ResourceFork "${ICON}" > /dev/null
+  then
+    echo "❌ Folder ${FILE} icon should have a resource fork, but doesn't."
+    exit 1
+  fi
+  echo "✅ Folder ${FILE} has an icon as expected."
 }
