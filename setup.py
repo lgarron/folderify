15aab04e633fd1316f5d3000564a2ebd9279cb99
Lucas Garron <code@garron.net>
2023-04-13 15:19:04 -0700

    Change binary name to `folderify-v2` to avoid clobbering other installations.
    This may break anyone using the old `pip3` install binary by name, but hopefully it should be easy to work around.
    
    Unfortunately `pip3` doesn't offer a better alternative:
    https://github.com/pypa/pip/issues/3980#issuecomment-1507684273

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         "console_scripts": [
-            "folderify = folderify.__main__:main",
+            "folderify-v2 = folderify.__main__:main",
         ],
     },
 )
